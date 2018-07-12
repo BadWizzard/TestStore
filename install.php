ec99b272db1a45eaaf304e21902d9704d202d968
BadWizzard <swicher123@gmail.om>
2018-07-12 17:13:27 +0300

    Deleted missed permissions.

--- install.php
+++ install.php
@@ -3,7 +3,7 @@
 // Set variables for our request
 $shop = $_GET['shop'];
 $api_key = "fd9ef66e747ba28b32924030a21a7ba8";
-$scopes = "read_orders,write_orders,read_draft_orders,write_draft_orders,read_products,write_products,read_product_listings,read_customers,write_customers,read_inventory,write_inventory,read_fulfillments,write_fulfillments,read_users,write_users,read_checkouts,write_checkouts,read_reports,write_reports,read_shopify_payments_payouts,read_resource_feedbacks,write_resource_feedbacks";
+$scopes = "read_orders,write_orders,read_draft_orders,write_draft_orders,read_products,write_products,read_product_listings,read_customers,write_customers,read_inventory,write_inventory,read_fulfillments,write_fulfillments,read_checkouts,write_checkouts,read_reports,write_reports,read_shopify_payments_payouts,read_resource_feedbacks,write_resource_feedbacks";
 $redirect_uri = "https://testmystore.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
